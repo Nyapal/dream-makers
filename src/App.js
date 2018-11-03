2ca3ca94b86be58ac410832057d8574045820dae
Jasmine Anderson <jazz2900@gmail.com>
2018-11-03 15:41:21 -0700

    updating to github, updated app view files

--- src/App.js
+++ src/App.js
@@ -11,17 +11,39 @@ class App extends Component {
   render() {
         console.log('render')
     return (
-        <div className="App">
-            <div class="search">
-                <h1>yparser</h1>
-                <input></input><br></br>
-                <button>Submit</button>
+    <div className="App">
+        <header>
+            <div class="container">
+                <h1 class="logo"></h1>
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                <nav>
             </div>
-            <div class="result">
+        </header>
+        <div class="body">
+            <div class="search-container">
+                <h1>yparser</h1>
+                <form class="search">
+                    <input></input><br></br>
+                    <button>Submit</button>
+                </form>
+            </div>
+            <div class="youtube"></div>
+            <form class="result-container">
                 <h2>Video Title</h2>
                 <p>text for video</p>
+            </form>
+            <div class="footer">
+                <ul>
+                    <li>Dream-Makers</li>
+                </ul>
             </div>
         </div>
+    </div>
         );
     }
 }
