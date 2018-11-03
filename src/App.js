40b9f7c58ba4f1ea9e883163eb2e9bb8f75af497
Jasmine Anderson <jazz2900@gmail.com>
2018-11-03 16:10:04 -0700

    added nav bar

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
                         <li><a href="#">About</a></li>
                         <li><a href="#">Contact</a></li>
                     </ul>
-                <nav>
+                </nav>
             </div>
         </header>
         <div class="body">
